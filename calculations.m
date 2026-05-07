8d93292d8d242f2a9e39c7f5c6a7e64efbad0ad7
tingkaihsu <tingkaihsu0924@gmail.com>
2026-05-08 00:43:46 +0800

    correct subtraction point
    correct subtraction point

--- calculations.m
+++ calculations.m
@@ -1,3 +1,5 @@
+(* ::Package:: *)
+
 (* ---------- coefficient helper ---------- *)
 
 del[a_, b_] := delCoeff @@ Sort[{a, b}];
@@ -27,7 +29,7 @@ ma = 0;
 (* stu symmetric expansion point *)
 Mlow[s_, t_, mA_, Nmax_Integer] :=
     gAAB^2 * (1/s + 1/t + 1/u) +
-    gAAA^2 * (1/(s-mA) + 1/(t-mA) + 1/(u-mA)) +
+    gAAA^2 * (1/(s-mA^2) + 1/(t-mA^2) + 1/(u-mA^2)) +
     Total[
         Function[{ab},
             del[ab[[1]], ab[[2]]]
@@ -39,7 +41,7 @@ Mlow[s_, t_, mA_, Nmax_Integer] :=
 (* Forward amplitude ansatz *)
 fwdMlow[s_, t_, mA_, Nmax_Integer] :=
     gAAB^2 * (1/s + 1/t + 1/u) +
-    gAAA^2 * (1/(s-mA) + 1/(t-mA) + 1/(u-mA)) +
+    gAAA^2 * (1/(s-mA^2) + 1/(t-mA^2) + 1/(u-mA^2)) +
     Total[
         Function[{ab},
             del[ab[[1]], ab[[2]]]
@@ -49,23 +51,32 @@ fwdMlow[s_, t_, mA_, Nmax_Integer] :=
     ] /. {u -> 4mA^2 - s - t};
 
 (* Kernel with subtraction points at s1 and s2 *)
-KerMlow[sp_, s_, t_, mA_, s1_, s2_, k_, Nmax_Integer] := Mlow[sp, t, mA, Nmax] /(sp) /((sp-s1)*(sp-s2))^(k/2);
+KerMlow[sp_, s_, t_, mA_, s1_, s2_, k_, Nmax_Integer] := Mlow[sp, t, mA, Nmax] /(sp-4mA^2/3) /((sp-s1)*(sp-s2))^(k/2);
 
-s1 = 0;
-s2 = -(t-4mA^2/3);
+s1 = 4mA^2/3;
+s2 = 8mA^2/3-t;
 
-Print["(twice-subtracted) stu-symmetric ansatz at s1 = 4mA^2/3, and s2 = 4mA^2-t-4mA^2/3 = ", SeriesCoefficient[-Residue[KerMlow[sp, s, t, mA, s1, s2, 2, 8], {sp, Infinity}], {t, 4mA^2/3, 2}]//FullSimplify];
+Print["(twice-subtracted) stu-symmetric ansatz at s1 = 4mA^2, and s2 = -(t-4mA^2/3) = ", SeriesCoefficient[-Residue[KerMlow[sp, s, t, mA, s1, s2, 2, 8], {sp, Infinity}], {t, 4mA^2/3, 2}]//FullSimplify];
 
-s1p = 0;
-s2p = -(t-4mA^2/3);
+s1p = 4mA^2/3;
+s2p = 8mA^2/3-t;
 
-Print["(forth-subtracted) stu-symmetric ansatz at s1 = 4mA^2/3, and s2 = 4mA^2-t-4mA^2/3 = ", SeriesCoefficient[-Residue[KerMlow[sp, s, t, mA, s1p, s2p, 4, 8], {sp, Infinity}], {t, 4mA^2/3, 0}]//FullSimplify];
+Print["(forth-subtracted) stu-symmetric ansatz at s1 = 4mA^2, and s2 = -(t-4mA^2/3) = ", SeriesCoefficient[-Residue[KerMlow[sp, s, t, mA, s1p, s2p, 4, 8], {sp, Infinity}], {t, 4mA^2/3, 0}]//FullSimplify];
+
+(* like the setup (2.3) in Extremal EFT, wich s-4mA^2/3, t-4mA^2/3, and u-4mA^2/3 *)
+Mtest[s_, t_, mA_] := gAAB^2 * (1/s + 1/t + 1/u) + gAAA^2 * (1/(s-mA^2) + 1/(t-mA^2) + 1/(u-mA^2)) + g0 + 
+					  g2*( (s-4mA^2/3)^2 + (t-4mA^2/3)^2 + (u-4mA^2/3)^2 ) + g4*( (s-4mA^2/3)^2 + (t-4mA^2/3)^2 + (u-4mA^2/3)^2 )^2 + 
+					  g3*( (s-4mA^2/3)*(t-4mA^2/3)*(u-4mA^2/3) ) + g5*( (s-4mA^2/3)^2 + (t-4mA^2/3)^2 + (u-4mA^2/3)^2 )*( (s-4mA^2/3)*(t-4mA^2/3)*(u-4mA^2/3) )+
+					  g6*( (s-4mA^2/3)^2 + (t-4mA^2/3)^2 + (u-4mA^2/3)^2 )^3 + g6p*((s-4mA^2/3)*(t-4mA^2/3)*(u-4mA^2/3))^2+g7*( (s-4mA^2/3)^2 + (t-4mA^2/3)^2 + (u-4mA^2/3)^2 )^2*((s-4mA^2/3)*(t-4mA^2/3)*(u-4mA^2/3))/.{u -> 4mA^2-s-t};
+
+KerMtest[sp_, s_, t_, mA_, s1_, s2_, k_] := Mtest[sp, t, mA] /(sp-4mA^2/3) /((sp-s1)*(sp-s2))^(k/2);
+
+s1 = 4mA^2/3;
+s2 = 8mA^2/3-t;
+
+Print["(twice-subtracted) test ansatz = ", Series[Limit[-Residue[KerMtest[sp, s, t, mA, s1, s2, 2], {sp, Infinity}],{s->4mA^2/3}], {t, 4mA^2/3, 2}]//FullSimplify//Normal];
+
+Print["(twice-subtracted) test ansatz = ", Series[Limit[-Residue[KerMtest[sp, s, t, mA, s1, s2, 4], {sp, Infinity}],{s->4mA^2/3}], {t, 4mA^2/3, 0}]//FullSimplify//Normal];
 
 
-Mtest[s_, t_, mA_] := gAAB^2 * (1/s + 1/t + 1/u) + gAAA^2 * (1/(s-mA) + 1/(t-mA) + 1/(u-mA)) + g0 + g2*( (s-4mA^2/3)^2 + (t-4mA^2/3)^2 + (u-4mA^2/3)^2 ) + g4*( (s-4mA^2/3)^2 + (t-4mA^2/3)^2 + (u-4mA^2/3)^2 )^2 + g3*( (s-4mA^2/3)*(t-4mA^2/3)*(u-4mA^2/3) ) + g5*( (s-4mA^2/3)^2 + (t-4mA^2/3)^2 + (u-4mA^2/3)^2 )*( (s-4mA^2/3)*(t-4mA^2/3)*(u-4mA^2/3) )/.{u -> 4mA^2-s-t};
 
-KerMtest[sp_, s_, t_, mA_, s1_, s2_, k_] := Mtest[sp, t, mA] /(sp) /((sp-s1)*(sp-s2))^(k/2);
-
-s1 = 0;
-s2 = -(t-4mA^2/3);
-Print["(twice-subtracted) test ansatz = ", Series[-Residue[KerMtest[sp, s, t, mA, s1, s2, 2], {sp, Infinity}], {t, 4mA^2/3, 2}]//FullSimplify//Normal];
