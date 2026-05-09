3e56ec00f4f26b887cdfff088bc1d0fc66de03a7
TK <tingkaihsu0924@gmail.com>
2026-05-09 16:54:46 +0800

    may 9th update null_generator is fine

--- test9/null_gen.m
+++ test9/null_gen.m
@@ -11,23 +11,23 @@ validTriples[Nmax_Integer] :=
 (* Commented-out single-term prototype kept for reference *)
 
 (* AAAA scattering *)
-Mlow[s_, t_, Nmax_Integer] :=
+Mfwdlow[s_, t_, mA_, Nmax_Integer] :=
     gAAB^2 * (1/s + 1/t + 1/u) +
-    gAAA^2 * (1/(s-1) + 1/(t-1) + 1/(u-1)) +
+    gAAA^2 * (1/(s-mA^2) + 1/(t-mA^2) + 1/(u-mA^2)) +
     Total[
         Function[{ab},
             del[ab[[1]], ab[[2]]]
-            * (s)^ab[[1]]
+            * (s-2mA^2)^ab[[1]]
             * (t)^ab[[2]]
         ] /@ validTriples[Nmax]
-    ] /. {u -> 4 - s - t};
+    ] /. {u -> 4mA^2 - s - t};
 
 
-(* Print["v[l,0] = ", v[l, 0]//FullSimplify]; *)
-(* 1 *)
+(* Test if the double contour still works *)
 
-(* Print["v[l,1] = ", v[l, 1]//FullSimplify]; *)
-(* l(l+1) *)
+DblCtrTest[mA_, k_Integer, q_Integer, Nmax_Integer] := Residue[ Residue[ 1/(s*t) ( Mfwdlow[s, t, mA, Nmax]/(s^(k-q)*t^q) - Mfwdlow[t, s, mA, Nmax]/(t^(k-q)*s^q) ), {s, Infinity}], {t, 0}];
+
+Print["Double contour test: ", DblCtrTest[mA, 5, 2, 10]];
 
 (* Null constraints *)
 
@@ -45,37 +45,19 @@ directPiece[k_Integer, q_Integer, J_, t_, sp_, mA_] :=
 crossPiece[k_Integer, q_Integer, J_, t_, sp_, mA_] :=
   kernelCross[k, q, t, sp, mA] * Sqrt[sp/(sp-4mA^2)] * LegendreP[J, 1 + 2 t/(sp-4mA^2)];
 
-directRes = Assuming[J ∈ Integers && J >= 0,
-  FullSimplify @ SeriesCoefficient[directPiece[10, 2, J, t, sp, mA], {t, 0, -1}]
+directRes[k_Integer, q_Integer] := Assuming[J ∈ Integers && J >= 0,
+  FullSimplify @ SeriesCoefficient[directPiece[k, q, J, t, sp, mA], {t, 0, -1}]
 ];
 
-crossRes = Assuming[J ∈ Integers && J >= 0,
-  FullSimplify @ SeriesCoefficient[crossPiece[10, 2, J, t, sp, mA], {t, 0, -1}]
+crossRes[k_Integer, q_Integer] := Assuming[J ∈ Integers && J >= 0,
+  FullSimplify @ SeriesCoefficient[crossPiece[k, q, J, t, sp, mA], {t, 0, -1}]
 ];
 
-combined = FullSimplify[directRes - crossRes];
+kn = 5;
+qn = 2;
+combined[k_Integer, q_Integer] := FullSimplify[directRes[k, q] - crossRes[k, q]];
 
-Print["Combined Piece: ", combined];
-
-(* to be safe, we should ONLY use q >= 2, and k - (k-3)/2 >= 0 -> k >= 6 *)
-(* k, q *)
-(* 5, 2 *)
-(* (J*(1 + J)*Sqrt[sp/(-4*mA^2 + sp)]*(-((-4 + J)*(-2 + J)*(3 + J)*(5 + J)) - ((-1 + J)*(2 + J)*(-4*mA^2 + sp)^3*(36*mA^2 + (-15 + J + J^2)*sp))/sp^4))/(36*(-4*mA^2 + sp)^6) *)
-
-(* 6, 2 *)
-(* (J*(1 + J)*Sqrt[sp/(-4*mA^2 + sp)]*(-((-6 + J)*(-4 + J)*(-2 + J)*(3 + J)*(5 + J)*(7 + J)) - ((-1 + J)*(2 + J)*(-4*mA^2 + sp)^3*(-2304*mA^4 + 1152*mA^2*sp + (-72 + J*(1 + J)*(-18 + J + J^2))*sp^2))/sp^5))/(576*(-4*mA^2 + sp)^7) *)
-
-(* 7, 2 *)
-(* (J*(1 + J)*Sqrt[sp/(-4*mA^2 + sp)]*(-(((-6 + J)*(-4 + J)*(-2 + J)*(3 + J)*(5 + J)*(7 + J)*(-47 + J + J^2))/(-4*mA^2 + sp)^8) + ((-1 + J)*(2 + J)*(((-4 + J)*(-3 + J)*(-2 + J)*(3 + J)*(4 + J)*(5 + J)*sp^3)/(4*mA^2 - sp)^5 + 3600/(-4*mA^2 + sp)^2))/sp^6))/14400 *)
-
-(* 8, 2 *)
-(* (J*(1 + J)*Sqrt[sp/(-4*mA^2 + sp)]*(((-8 + J)*(-6 + J)*(-4 + J)*(-2 + J)*(3 + J)*(5 + J)*(7 + J)*(9 + J)*(-38 + J + J^2))/(4*mA^2 - sp)^9 + ((-1 + J)*(2 + J)*(-(((-5 + J)*(-4 + J)*(-3 + J)*(-2 + J)*(3 + J)*(4 + J)*(5 + J)*(6 + J)*sp^4)/(-4*mA^2 + sp)^6) + 129600/(-4*mA^2 + sp)^2))/sp^7))/518400 *)
-
-(* 9, 2 *)
-(* (J*(1 + J)*Sqrt[sp/(-4*mA^2 + sp)]*(-(((-8 + J)*(-6 + J)*(-4 + J)*(-2 + J)*(3 + J)*(5 + J)*(7 + J)*(9 + J)*(2754 + J*(1 + J)*(-119 + J + J^2)))/(-4*mA^2 + sp)^10) + ((-1 + J)*(2 + J)*(((-6 + J)*(-5 + J)*(-4 + J)*(-3 + J)*(-2 + J)*(3 + J)*(4 + J)*(5 + J)*(6 + J)*(7 + J)*sp^5)/(4*mA^2 - sp)^7 + 6350400/(-4*mA^2 + sp)^2))/sp^8))/25401600 *)
-
-(* 10, 2 *)
-
+Print["Combined Piece: ", combined[kn, qn]];
 (* large J limit *)
 
-Print["Large J limit: ", SeriesCoefficient[combined, {J, 0, 16}]];
+Print["Large J limit: ", Limit[combined[kn, qn]/J^6, {J -> Infinity}]];
