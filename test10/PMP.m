fda4bcf47025406f44147af7c7240ffd03f3bc1d
tingkaihsu <tingkaihsu0924@gmail.com>
2026-05-20 15:27:56 +0800

    bug fixed

--- test10/PMP.m
+++ test10/PMP.m
@@ -68,8 +68,10 @@ M1[x_?NumericQ] := {
   {x^2/2, x^4/12 + 2 x^2}
 };
 
-
-f1List = {1+x^4, x^4/12 + x^2};  (* column 1: entries (1,1) of M0 and M1 *)
+f1List = {
+  Function[{x}, M0[x][[1, 1]]],
+  Function[{x}, M1[x][[1, 1]]]
+};
 
 Print["f1List = ", f1List];
 
@@ -123,10 +125,16 @@ testNumericalSDP[spFile_String, jsonFile_String, prec_:650] := Module[
       (* TWO outer braces = JSON levels 1 & 2.
          Table produces level-3 list: {{f1(xi,Jj)}, {f2(xi,Jj)}, {f3(xi,Jj)}}.
          Each {fList[[k]][...]} is the level-4 coefficient list (degree-0: 1 entry). *)
-      "polynomials" -> {{ Table[
-        {SetPrecision[f1List[[k]][samplePoints[[i]]], prec]},
-        {k, Length[f1List]}
-      ] }}
+      "polynomials" -> {
+        {  (* column s=1 *)
+          Table[{SetPrecision[f1List[[k]][samplePoints[[i]]], prec]}, {k, Length[f1List]}],  (* row r=1: entry (1,1) *)
+          Table[{SetPrecision[f2List[[k]][samplePoints[[i]]], prec]}, {k, Length[f2List]}]   (* row r=2: entry (2,1) *)
+        },
+        {  (* column s=2 *)
+          Table[{SetPrecision[f3List[[k]][samplePoints[[i]]], prec]}, {k, Length[f3List]}],  (* row r=1: entry (1,2) *)
+          Table[{SetPrecision[f4List[[k]][samplePoints[[i]]], prec]}, {k, Length[f4List]}]   (* row r=2: entry (2,2) *)
+        }
+      }
     |>],
     {i, Length[samplePoints]}
   ];
@@ -134,7 +142,7 @@ testNumericalSDP[spFile_String, jsonFile_String, prec_:650] := Module[
   (* Flatten polsRegular (2D Table \[RightArrow] flat list) *)
   WritePmpJsonNumerical[
     jsonFile,
-    SDP[obj, norm, Join[Flatten[polsRegular], polsExtra]],
+    SDP[obj, norm, Flatten[polsRegular] ],
     prec
   ];
   Print["Wrote PMP JSON to ", jsonFile];
