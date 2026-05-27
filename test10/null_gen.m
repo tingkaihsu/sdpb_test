d459aeca956457cd998d0ebeff5c14dd1d15ad7a
tingkaihsu <tingkaihsu0924@gmail.com>
2026-05-27 10:03:15 +0800

    null_gen.m: add solves, use FullSimplify, kn=4
    Add Solve expressions (solABAB, solBBAA) with prints to expose cos solutions for the MABAB/MBBAA t-relations. Replace Simplify with FullSimplify and include m >= 0 in the Assuming clauses for sKer and uKer to strengthen simplification. Change kn from 5 to 4 and replace the previous Limit-based massless check with a direct m->0 evaluation; also add/adjust diagnostic Print statements and minor formatting tweaks.

--- test10/null_gen.m
+++ test10/null_gen.m
@@ -70,27 +70,37 @@ s2[mA_] := 2mA^2/3;
 Print["double contour test (stu ansatz): ", Residue[Residue[ Ker[s,t,s1[m],s2[m],5,2]*stuMABAB[s,t,m,10]-Ker[t,s,s1[m],s2[m],5,2]*stuMBBAA[s,t,m,10], {s,Infinity}], {t,2m^2/3} ]//FullSimplify ]
 
 
-(* NOT sure about the 1/2 factor in MABAB *)
+solABAB = Solve[t == ((s-m^2)^2(cos-1))/(2s),{cos}]//FullSimplify;
+Print["solABAB = ", solABAB];
+
+solBBAA = Solve[t == m^2-s/2+1/2*Sqrt[s*(s-4m^2)]*cos,{cos}]//FullSimplify;
+Print["solBBAA = ", solBBAA];
+
+(* Note that the 1/2 factor in MABAB *)
 MABAB[s_, t_, m_] := 1/2*s/(s-m^2)*LegendreP[J, 1+(2*s*t)/(s-m^2)^2];
 MBBAA[s_, t_, m_] := (s/(s-m^2))^(1/4)*LegendreP[J,(2t+s-2m^2)/Sqrt[s(s-4m^2)]];
 
-sKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0,
-  Simplify @ SeriesCoefficient[Ker[sp,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,sp,s1[m],s2[m],k,q]*MBBAA[sp,t,m], {t, 2m^2/3, -1}]
+sKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0 && m >= 0,
+  FullSimplify @ SeriesCoefficient[Ker[sp,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,sp,s1[m],s2[m],k,q]*MBBAA[sp,t,m], {t, 2m^2/3, -1}]
 ];
 
-uKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0,
-  Simplify @ SeriesCoefficient[Ker[2m^2-sp-t,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,2m^2-sp-t,s1[m],s2[m],k,q]*MBBAA[sp,t,m], {t, 2m^2/3, -1}]
+uKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0 && m >= 0,
+  FullSimplify @ SeriesCoefficient[Ker[2m^2-sp-t,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,2m^2-sp-t,s1[m],s2[m],k,q]*MBBAA[sp,t,m], {t, 2m^2/3, -1}]
 ];
 
 Xkq[k_Integer, q_Integer, J_, sp_, m_] := sKer[sp,m,J,k,q]-uKer[sp,m,J,k,q];
 
-kn = 5;
+kn = 4;
 qn = 2;
 
-Print["X[5,2] = ", Xkq[kn,qn,J,sp,m]//Simplify];
-Print["massless-limit X[5,2] = ", Limit[Xkq[kn,qn,J,sp,m],{m -> 0}, Direction->"FromAbove"]];
+Print["X[4,2] = ", Xkq[kn,qn,J,sp,m]//Simplify];
 
+
+
+Print["massless-limit X[4,2] = ", Xkq[kn,qn,J,sp,0]];
 
 (4-5*d)J*(J+1)+2(J*(J+1))^2/.{d -> 4}//FullSimplify
 (23d^2-12d-20)*J*(J+1)+(-21d-2)*(J*(J+1))^2+4(J*(J+1))^3/.{d->4}//FullSimplify
+
+
 
