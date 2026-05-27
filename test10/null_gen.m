82a529a5efb28aea360172bc600a9a84c39cec58
tingkaihsu <tingkaihsu0924@gmail.com>
2026-05-27 11:18:51 +0800

    Update null_gen: symmetric ansatz & notebook
    Refactor null_gen: modify the ABAB/BBAA ansatzes to use explicit symmetry-aware names (suMABAB, tuMBBAA), remove unused gp/cp aliases and use unified coefficient accessors, and adjust polynomial bases to enforce the intended s/u or t/u symmetries. Change kernel expansion points (series evaluated at m^2 instead of 2m^2/3), bump kn from 4 to 5, and add Residue checks. Add the accompanying Mathematica notebook test10/null_gen.nb and remove obsolete files (Untitled-1.nb, type-II_string_amp.m, type-II_string_amp_fixed.m). Minor print/debug output updated to reflect the new parameters.

--- test10/null_gen.m
+++ test10/null_gen.m
@@ -6,8 +6,6 @@ ClearAll["Global`*"];
 
 g[a_, b_] := gABAB @@ Sort[{a, b}];
 c[a_, b_] := cABAB @@ Sort[{a, b}];
-gp[a_, b_] := gBBAA @@ Sort[{a, b}];
-cp[a_, b_] := cBBAA @@ Sort[{a, b}];
 
 validTriples[Nmax_Integer] :=
     Flatten[Table[{a, b}, {a, 0, Nmax}, {b, 0, Nmax - a}], 1];
@@ -17,7 +15,7 @@ validTriples[Nmax_Integer] :=
 (* Commented-out single-term prototype kept for reference *)
 
 (* ABAB scattering change the ansatz to be s-u symmetric *)
-fwdMABAB[s_, t_, mA_, Nmax_Integer] :=
+suMABAB[s_, t_, mA_, Nmax_Integer] :=
     gABB^2 * (1/s + 1/u) + gAAB*gBBB * (1/t) + 
     gAAB^2 * (1/(s-mA^2) + 1/(u-mA^2)) + gAAA*gBBA * (1/(t-mA^2)) +
     Total[
@@ -27,47 +25,27 @@ fwdMABAB[s_, t_, mA_, Nmax_Integer] :=
             * (u-mA^2)^ab[[2]]
         ] /@ validTriples[Nmax]
     ] /. {u -> 2mA^2 - s - t};
-    
-stuMABAB[s_, t_, mA_, Nmax_Integer] :=
-    gABB^2 * (1/s + 1/u) + gAAB*gBBB * (1/t) + 
-    gAAB^2 * (1/(s-mA^2) + 1/(u-mA^2)) + gAAA*gBBA * (1/(t-mA^2)) +
-    Total[
-        Function[{ab},
-            c[ab[[1]], ab[[2]]]
-            * (s-2mA^2/3)^ab[[1]]
-            * (u-2mA^2/3)^ab[[2]]
-        ] /@ validTriples[Nmax]
-    ] /. {u -> 2mA^2 - s - t};
 
 (* there is no physical meaning of forward limit for BBAA *)
-fwdMBBAA[s_, t_, mA_, Nmax_Integer] :=
+(* we should use a u <-> t symmetric ansatz*)
+
+tuMBBAA[s_, t_, mA_, Nmax_Integer] :=
     gBBB*gAAB * (1/s + 1/u) + gBBA^2 * (1/t) +
     gBBA*gAAA * (1/(s-mA^2) + 1/(u-mA^2)) + gBAA^2 * (1/(t-mA^2)) +
     Total[
         Function[{ab},
-            gp[ab[[1]], ab[[2]]]
-            * (s-mA^2)^ab[[1]]
+            c[ab[[1]], ab[[2]]]
+            * (t-mA^2)^ab[[1]]
             * (u-mA^2)^ab[[2]]  
         ] /@ validTriples[Nmax]
     ] /. {u -> 2mA^2 - s - t};
 
-stuMBBAA[s_, t_, mA_, Nmax_Integer] :=
-    gBBB*gAAB * (1/s + 1/u) + gBBA^2 * (1/t) +
-    gBBA*gAAA * (1/(s-mA^2) + 1/(u-mA^2)) + gBAA^2 * (1/(t-mA^2)) +
-    Total[
-        Function[{ab},
-            cp[ab[[1]], ab[[2]]]
-            * (t-2mA^2/3)^ab[[1]]
-            * (u-2mA^2/3)^ab[[2]]  
-        ] /@ validTriples[Nmax]
-    ] /. {u -> 2mA^2 - s - t};
-
+s1[m_] := 2m^2;
+s2[m_] := 0;
 Ker[s_, t_, s1_, s2_, k_, q_] := 1/( (s-s1)*(t-s2) ) * 1/(s-s1)^(k-q) * 1/(t-s2)^q;
 
-s1[mA_] := 2mA^2/3;
-s2[mA_] := 2mA^2/3;
-
-Print["double contour test (stu ansatz): ", Residue[Residue[ Ker[s,t,s1[m],s2[m],5,2]*stuMABAB[s,t,m,10]-Ker[t,s,s1[m],s2[m],5,2]*stuMBBAA[s,t,m,10], {s,Infinity}], {t,2m^2/3} ]//FullSimplify ]
+Residue[ Residue[ Ker[s,t,s1[m],s2[m],5,2]*suMABAB[s,t,m,10], {s,Infinity}] , {t,0}]//Simplify
+Residue[ Residue[ Ker[t,s,s1[m],s2[m],5,2]*tuMBBAA[s,t,m,10], {s,Infinity}] , {t,0}]//Simplify
 
 
 solABAB = Solve[t == ((s-m^2)^2(cos-1))/(2s),{cos}]//FullSimplify;
@@ -82,25 +60,27 @@ MABAB[s_, t_, m_] := 1/2*s/(s-m^2)*LegendreP[J, 1+(2*s*t)/(s-m^2)^2];
 MBBAA[s_, t_, m_] := (s/(s-m^2))^(1/4)*LegendreP[J,(2t+s-2m^2)/Sqrt[s(s-4m^2)]];
 
 sKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0,
-  Simplify @ SeriesCoefficient[Ker[sp,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,sp,s1[m],s2[m],k,q]*MBBAA[sp,t,m], {t, 2m^2/3, -1}]
+  Simplify @ SeriesCoefficient[Ker[sp,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,sp,s1[m],s2[m],k,q]*MBBAA[sp,t,m], {t, m^2, -1}]
 ];
 
 uKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0,
-  Simplify @ SeriesCoefficient[Ker[2m^2-sp-t,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,2m^2-sp-t,s1[m],s2[m],k,q]*MBBAA[2m^2-sp-t,t,m], {t, 2m^2/3, -1}]
+  Simplify @ SeriesCoefficient[Ker[2m^2-sp-t,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,2m^2-sp-t,s1[m],s2[m],k,q]*MBBAA[2m^2-sp-t,t,m], {t, m^2, -1}]
 ];
 
 Xkq[k_Integer, q_Integer, J_, sp_, m_] := sKer[sp,m,J,k,q]-uKer[sp,m,J,k,q];
 
-kn = 4;
+kn = 5;
 qn = 2;
 
-Print["X[4,2] = ", Xkq[kn,qn,J,sp,m]//Simplify];
+Print["X[5,2] = ", Xkq[kn,qn,J,sp,m]//Simplify];
 
 
 
-Print["massless-limit X[4,2] = ", FullSimplify[Xkq[kn,qn,J,sp,0],Assumptions->{sp>0}]];
+Print["massless-limit X[5,2] = ", FullSimplify[Xkq[kn,qn,J,sp,0],Assumptions->{sp>0}]];
 (4-5*d)J*(J+1)+2(J*(J+1))^2/.{d -> 4}//FullSimplify
 (23d^2-12d-20)*J*(J+1)+(-21d-2)*(J*(J+1))^2+4(J*(J+1))^3/.{d->4}//FullSimplify
+
+
 
 
 
