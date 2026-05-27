261df1c57a3b6e80f22ce067705cc0c16d02cc79
tingkaihsu <tingkaihsu0924@gmail.com>
2026-05-27 10:26:00 +0800

    Update null_gen.m

--- test10/null_gen.m
+++ test10/null_gen.m
@@ -57,7 +57,7 @@ stuMBBAA[s_, t_, mA_, Nmax_Integer] :=
     Total[
         Function[{ab},
             cp[ab[[1]], ab[[2]]]
-            * (s-2mA^2/3)^ab[[1]]
+            * (t-2mA^2/3)^ab[[1]]
             * (u-2mA^2/3)^ab[[2]]  
         ] /@ validTriples[Nmax]
     ] /. {u -> 2mA^2 - s - t};
@@ -78,14 +78,15 @@ Print["solBBAA = ", solBBAA];
 
 (* Note that the 1/2 factor in MABAB *)
 MABAB[s_, t_, m_] := 1/2*s/(s-m^2)*LegendreP[J, 1+(2*s*t)/(s-m^2)^2];
+(* BB -> AA scattering is NOT s <-> symmetric? *)
 MBBAA[s_, t_, m_] := (s/(s-m^2))^(1/4)*LegendreP[J,(2t+s-2m^2)/Sqrt[s(s-4m^2)]];
 
-sKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0 && m >= 0,
-  FullSimplify @ SeriesCoefficient[Ker[sp,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,sp,s1[m],s2[m],k,q]*MBBAA[sp,t,m], {t, 2m^2/3, -1}]
+sKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0,
+  Simplify @ SeriesCoefficient[Ker[sp,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,sp,s1[m],s2[m],k,q]*MBBAA[sp,t,m], {t, 2m^2/3, -1}]
 ];
 
-uKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0 && m >= 0,
-  FullSimplify @ SeriesCoefficient[Ker[2m^2-sp-t,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,2m^2-sp-t,s1[m],s2[m],k,q]*MBBAA[sp,t,m], {t, 2m^2/3, -1}]
+uKer[sp_,m_,J_,k_,q_] := Assuming[J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0,
+  Simplify @ SeriesCoefficient[Ker[2m^2-sp-t,t,s1[m],s2[m],k,q]*MABAB[sp,t,m]-Ker[t,2m^2-sp-t,s1[m],s2[m],k,q]*MBBAA[2m^2-sp-t,t,m], {t, 2m^2/3, -1}]
 ];
 
 Xkq[k_Integer, q_Integer, J_, sp_, m_] := sKer[sp,m,J,k,q]-uKer[sp,m,J,k,q];
@@ -97,8 +98,7 @@ Print["X[4,2] = ", Xkq[kn,qn,J,sp,m]//Simplify];
 
 
 
-Print["massless-limit X[4,2] = ", Xkq[kn,qn,J,sp,0]];
-
+Print["massless-limit X[4,2] = ", FullSimplify[Xkq[kn,qn,J,sp,0],Assumptions->{sp>0}]];
 (4-5*d)J*(J+1)+2(J*(J+1))^2/.{d -> 4}//FullSimplify
 (23d^2-12d-20)*J*(J+1)+(-21d-2)*(J*(J+1))^2+4(J*(J+1))^3/.{d->4}//FullSimplify
 
