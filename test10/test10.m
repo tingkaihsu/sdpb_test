07c757a769e2376f125f8530b991236d21bb6659
tingkaihsu <tingkaihsu0924@gmail.com>
2026-05-29 15:04:01 +0800

    Update mass value in numerical calculations to improve precision

--- test10/test10.m
+++ test10/test10.m
@@ -53,7 +53,7 @@ WritePmpJsonNumerical[
 (* problem-specific *)
 (* let the mass be 4mA^2 < M^2 = 1 where M  = 1 to infinity *)
 
-maVal = N[1/100, 600];
+maVal = N[1/1000, 600];
 
 Print["mA = ", maVal]
 
