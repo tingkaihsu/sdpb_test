4371ca80cb208fe720d4d3bae8d73a4450155aef
tingkaihsu <tingkaihsu0924@gmail.com>
2026-05-29 14:08:09 +0800

    Enhance numerical functions and update notebook content
    - Added new functions X52ABBA, X52BBAA, and X52ABAB for improved calculations.
    - Refactored existing functions to streamline calculations and improve readability.
    - Updated notebook metadata and cell labels for consistency.
    - Adjusted output formatting and cell change times in the notebook.
    - Introduced new matrix functions M41, M42, M43, M51, M52, M53, M61, and M62 to accommodate new calculations.
    - Enhanced handling of large J limits in functions X62AAAA and X62BBBB.

--- test10/null_gen.m
+++ test10/null_gen.m
@@ -44,47 +44,78 @@ s1[m_,t_] := m^2;
 s2[m_,t_] := 4m^2-t-s1[m,t];
 Ker[s_, t_, s1_, s2_, k_] := 1/(s-s1)*1/((s-s1)*(s-s2))^(k/2);
 
-Print["g[4,0] = ", SeriesCoefficient[ Residue[Ker[s,t,s1[m,t],s2[m,t],2]*suMABBA[s,t,m,10],{s,s1[m,t]}]+Residue[Ker[s,t,s1[m,t],s2[m,t],2]*suMABBA[s,t,m,10],{s,s2[m,t]}] ,{t,0,2}]//FullSimplify]
-Print["g[4,0] = ", SeriesCoefficient[ Residue[Ker[s,t,s1[m,t],s2[m,t],4]*suMABBA[s,t,m,10],{s,s1[m,t]}]+Residue[Ker[s,t,s1[m,t],s2[m,t],4]*suMABBA[s,t,m,10],{s,s2[m,t]}] ,{t,0,0}]//FullSimplify]
+Print["gABBA[4,0] = ", SeriesCoefficient[ Residue[Ker[s,t,s1[m,t],s2[m,t],2]*suMABBA[s,t,m,10],{s,s1[m,t]}]+Residue[Ker[s,t,s1[m,t],s2[m,t],2]*suMABBA[s,t,m,10],{s,s2[m,t]}] ,{t,0,2}]//FullSimplify]
+Print["gABBA[4,0] = ", SeriesCoefficient[ Residue[Ker[s,t,s1[m,t],s2[m,t],4]*suMABBA[s,t,m,10],{s,s1[m,t]}]+Residue[Ker[s,t,s1[m,t],s2[m,t],4]*suMABBA[s,t,m,10],{s,s2[m,t]}] ,{t,0,0}]//FullSimplify]
 
 (* Note that the 1/2 factor in MABAB *)
-MABAB[s_, t_, m_] := 1/2*s/(s-m^2)*LegendreP[J, (-m^4+s (-2 m^2+s+2 t))/(m^2-s)^2];
+MABAB[s_, t_, m_] := 1/2*s/(s-m^2)*LegendreP[J, (2*s*(2m^2-s-t))/(m^2-s)^2];
 (* Note that the 1/2 factor in MABBA *)
 MABBA[s_,t_,m_] := 1/2*s/(s-m^2)*LegendreP[J, 1+(2 s t)/(m^2-s)^2];
 (* BB -> AA scattering is NOT s <-> t symmetric? *)
-MBBAA[s_, t_, m_] := (s/(s-4m^2))^(1/4)*LegendreP[J,(-2 m^2+s+2 t)/Sqrt[s (-4 m^2+s)]];
+MBBAA[s_, t_, m_] := s/Sqrt[s(s-4m^2)]*LegendreP[J,(2m^2-s-t)/Sqrt[s (-4 m^2+s)]];
 
 (* ---------- partial-wave kernels around crossing point ---------- *)
 
 
-(* AB -> AB s-channel *)
-sKer[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[ 1/((sp-m^2)*t)*1/((sp-m^2)^(k-q)*t^q) * MABBA[sp, t, m] - 1/((sp-m^2)*t)*1/((sp-m^2)^q*t^(k-q)) * MBBAA[sp, t, m], {t, 0, -1} ] ];
+(* AB -> BA s-channel *)
+sKerABBA[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[ 1/((sp-m^2)*t)*1/((sp-m^2)^(k-q)*t^q) * MABBA[sp, t, m] - 1/((sp-m^2)*t)*1/((sp-m^2)^q*t^(k-q)) * MBBAA[sp, t, m], {t, 0, -1} ] ];
 
 (* crossed u-channel contribution *)
-uKer[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[ 1/(((2m^2-sp-t)-m^2)*t)*1/(((2m^2-sp-t)-m^2)^(k-q)*t^q) * MABBA[sp, t, m] - 1/(((2m^2-sp-t)-m^2)*t)*1/(((2m^2-sp-t)-m^2)^q*t^(k-q)) * MABAB[sp, t, m], {t, 0, -1} ] ];
+uKerABBA[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[ 1/(((2m^2-sp-t)-m^2)*t)*1/(((2m^2-sp-t)-m^2)^(k-q)*t^q) * MABBA[sp, t, m] - 1/(((2m^2-sp-t)-m^2)*t)*1/(((2m^2-sp-t)-m^2)^q*t^(k-q)) * MABAB[sp, t, m], {t, 0, -1} ] ];
 
 (* null kernel *)
-Xkq[k_Integer, q_Integer, J_, sp_, m_] := Simplify[ sKer[sp, m, J, k, q] - uKer[sp, m, J, k, q] ];
+XkqABBA[k_Integer, q_Integer, J_, sp_, m_] := Simplify[ sKerABBA[sp, m, J, k, q] - uKerABBA[sp, m, J, k, q] ];
   
   
 k1 = 5;
 q1 = 2;
 
-Print["X[5,2] = ", Xkq[k1,q1,J,sp,m]//Simplify];
+Print["XABBA[5,2] = ", XkqABBA[k1,q1,J,sp,m]//Simplify];
+Print[""]
 
 k2 = 4;
 q2 = 2;
 
-Print["X[4,2] = ", Xkq[k2,q2,J,sp,m]//Simplify];
+Print["XABBA[4,2] = ", XkqABBA[k2,q2,J,sp,m]//Simplify];
+Print[""]
+
 
 (* term by term, n4ABBA *)
-n4ABBA[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[1/((sp-m^2)*t)*1/((sp-m^2)^(k-q)*t^q) * MABBA[sp, t, m]-1/(((2m^2-sp-t)-m^2)*t)*1/(((2m^2-sp-t)-m^2)^(k-q)*t^q) * MABBA[sp, t, m], {t,0,-1}] ];
-n4BBAA[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[- (1/((sp-m^2)*t))*1/((sp-m^2)^q*t^(k-q)) * MBBAA[sp, t, m], {t,0,-1}] ];
-n4ABAB[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[1/(((2m^2-sp-t)-m^2)*t)*1/(((2m^2-sp-t)-m^2)^q*t^(k-q)) * MABAB[sp, t, m], {t,0,-1}] ];
+XkqABBA[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[1/((sp-m^2)*t)*1/((sp-m^2)^(k-q)*t^q) * MABBA[sp, t, m]-1/(((2m^2-sp-t)-m^2)*t)*1/(((2m^2-sp-t)-m^2)^(k-q)*t^q) * MABBA[sp, t, m], {t,0,-1}] ];
+XkqBBAA[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[- (1/((sp-m^2)*t))*1/((sp-m^2)^q*t^(k-q)) * MBBAA[sp, t, m], {t,0,-1}] ];
+XkqABAB[sp_, m_, J_, k_, q_] := Assuming[ J \[Element] Integers && J >= 0 && m >= 0 && sp >= 0, Simplify @ SeriesCoefficient[1/(((2m^2-sp-t)-m^2)*t)*1/(((2m^2-sp-t)-m^2)^q*t^(k-q)) * MABAB[sp, t, m], {t,0,-1}] ];
 
-Print["n4ABBA[4,2] = ", n4ABBA[sp,mA,J,k2,q2]//Simplify]
-Print["n4BBAA[4,2] = ", n4BBAA[sp,mA,J,k2,q2]//Simplify]
-Print["n4ABAB[4,2] = ", n4ABAB[sp,mA,J,k2,q2]//Simplify]
+Print["XkqABBA[4,2] = ", XkqABBA[sp,mA,J,k2,q2]//Simplify]
+Print[""]
+Print["XkqBBAA[4,2] = ", XkqBBAA[sp,mA,J,k2,q2]//Simplify]
+Print[""]
+Print["XkqABAB[4,2] = ", XkqABAB[sp,mA,J,k2,q2]//Simplify]
+
+Print["XkqABBA[5,2] = ", XkqABBA[sp,mA,J,k1,q1]//Simplify]
+Print[""]
+Print["XkqBBAA[5,2] = ", XkqBBAA[sp,mA,J,k1,q1]//Simplify]
+Print[""]
+Print["XkqABAB[5,2] = ", XkqABAB[sp,mA,J,k1,q1]//Simplify]
+
+(* AAAA scattering null constraint *)
+
+MAAAA[sp_,t_,mA_,J_]:= Sqrt[sp/(sp-4mA^2)] * LegendreP[J, 1 + 2 t/(sp-4mA^2)];
+
+sKerAAAA[k_Integer, q_Integer, sp_, mA_, J_] := Assuming[ J \[Element] Integers && J >= 0 && mA >= 0 && sp >= 0, Simplify @ SeriesCoefficient[1/t (1/(t^q sp^(k - q + 1)) - 1/(t^(k - q) sp^(q + 1))) * MAAAA[sp,t,mA,J], {t,0,-1}]];
+
+uKerAAAA[k_Integer, q_Integer, sp_, mA_, J_] := Assuming[ J \[Element] Integers && J >= 0 && mA >= 0 && sp >= 0, Simplify @ SeriesCoefficient[1/t (1/(t^q (4mA^2 -sp -t)^(k - q + 1)) - 1/(t^(k - q) (4mA^2 -sp -t)^(q + 1))) * MAAAA[sp,t,mA,J], {t,0,-1}]];
+XkqAAAA[k_Integer, q_Integer, sp_, mA_, J_] := Assuming[ J \[Element] Integers && J >= 0 && mA >= 0 && sp >= 0, Simplify @ sKerAAAA[k,q,sp,mA,J]-uKerAAAA[k,q,sp,mA,J]];
+
+Print["XAAAA[5,2] = ", XkqAAAA[k1,q1,sp,mA,J]]
+
+Print["massless XAAAA[5,2] = ", FullSimplify[Limit[XkqAAAA[k1,q1,sp,mA,J], mA->0, Direction->"FromAbove"],Assumptions->{sp>=0}] ]
+
+Print["XAAAA[4,2] = ", XkqAAAA[k2,q2,sp,mA,J]]
+
+Print["XAAAA[6,2] = ", XkqAAAA[6,2,sp,mA,J]]
+
+Print["XBBBB[6,2] = ", FullSimplify[Limit[XkqAAAA[6,2,sp,mA,J],mA->0, Direction->"FromAbove"],Assumptions->{sp>=0}]]
+Print["XAAAA[6,3] = ", XkqAAAA[6,3,sp,mA,J]]
 
 
 
--- test10/test10.m
+++ test10/test10.m
@@ -114,22 +114,61 @@ n4ABAB[x_?NumericQ, J_?IntegerQ] := Module[{sp, mA, result},
   Re[N[result, 600] ]
 ];
 
+
+X52ABBA[x_?NumericQ, J_?IntegerQ] := Module[{sp, mA, result}, 
+  sp = N[1/(1-x), 600];
+  mA = N[maVal, 600];
+  result = (sp (5 mA^2+(-5+2 J+2 J^2) sp))/(mA^2-sp)^8;
+  Re[N[result, 600] ]
+];
+
+X52BBAA[x_?NumericQ, J_?IntegerQ] := Module[{sp, mA,result},
+  sp = N[1/(1-x), 600];
+  mA = N[maVal, 600];
+  result = ((Sqrt[sp/(4 mA^2-sp)] LegendreP[J,3,2,(2 mA^2-sp)/Sqrt[sp (-4 mA^2+sp)]])/(48 mA^6 (mA^2-sp)^3));
+  Re[N[result, 600] ]
+];
+
+X52ABAB[x_?NumericQ, J_?IntegerQ] := Module[{sp, mA,result},
+  sp = N[1/(1-x), 600];
+  mA = N[maVal, 600];
+  result = 1/(3 (mA^2-sp)^6 (-mA^2+sp)) sp (15 LegendreP[J,(2 (2 mA^2-sp) sp)/(mA^2-sp)^2]+(18 (mA^2-sp) sp LegendreP[J,1,2,(2 (2 mA^2-sp) sp)/(mA^2-sp)^2])/Sqrt[mA^8-4 mA^6 sp-10 mA^4 sp^2+12 mA^2 sp^3-3 sp^4]+(9 (mA^2-sp)^2 sp^2 LegendreP[J,2,2,(2 (2 mA^2-sp) sp)/(mA^2-sp)^2])/(mA^8-4 mA^6 sp-10 mA^4 sp^2+12 mA^2 sp^3-3 sp^4)+(2 (mA^2-sp)^3 sp^3 LegendreP[J,3,2,(2 (2 mA^2-sp) sp)/(mA^2-sp)^2])/(mA^8-4 mA^6 sp-10 mA^4 sp^2+12 mA^2 sp^3-3 sp^4)^(3/2));
+  Re[N[result, 600] ]
+];
+
+
 X52AAAA[x_?NumericQ, J_?IntegerQ] := Module[{sp, mA},
   sp = N[1/(1-x), 600];
   mA = N[maVal, 600];    (* FIX 1: exact rational *)
   N[(J*(1 + J)*Sqrt[sp/(-4*mA^2 + sp)]*(-((-4 + J)*(-2 + J)*(3 + J)*(5 + J)) - ((-1 + J)*(2 + J)*(-4*mA^2 + sp)^3*(36*mA^2 + (-15 + J + J^2)*sp))/sp^4))/(36*(-4*mA^2 + sp)^6), 600]
 ];
 
-(* X52BBBB[x_?NumericQ, J_?IntegerQ] := Module[{sp},
+X52BBBB[x_?NumericQ, J_?IntegerQ] := Module[{sp},
   sp = N[1/(1-x), 600];
-  N[, 600]
-]; *)
+  N[-((J (1+J) (150+J (1+J) (-43+2 J (1+J))))/(36 sp^6)), 600]
+];
 
-(* Large J limit *)
+X62AAAA[x_?NumericQ, J_?IntegerQ] := Module[{sp, mA},
+  sp = N[1/(1-x), 600];
+  mA = N[maVal, 600];    (* FIX 1: exact rational *)
+  N[(J*(1 + J)*Sqrt[sp/(-4*mA^2 + sp)]*(-((-4 + J)*(-2 + J)*(3 + J)*(5 + J)) - ((-1 + J)*(2 + J)*(-4*mA^2 + sp)^3*(36*mA^2 + (-15 + J + J^2)*sp))/sp^4))/(36*(-4*mA^2 + sp)^6), 600]
+];
+
+X62BBBB[x_?NumericQ, J_?IntegerQ] := Module[{sp},
+  sp = N[1/(1-x), 600];
+  N[-(((-3+J) J (1+J) (4+J) (204+J (1+J) (-32+J+J^2)))/(288 sp^7)), 600]
+];
+
+(* Large J limit k = 6, q = 2 *)
 LargeJAAAA[x_?NumericQ] := Module[{sp, mA},
   sp = N[1/(1-x), 600];
   mA = N[maVal, 600];    (* FIX 1: exact rational *)
-  N[(Sqrt[sp/(-4*mA^2 + sp)]*(-32*mA^6 + 24*mA^4*sp - 6*mA^2*sp^2 + sp^3))/(18*sp^3*(-4*mA^2 + sp)^6), 600]
+  N[-(((1/(-4 mA^2 + sp))^(15/2) (-32 mA^6 + 24 mA^4 sp - 6 mA^2 sp^2 + sp^3))/(288 sp^(5/2))), 600]
+];
+
+LargeJBBBB[x_?NumericQ] := Module[{sp},
+  sp = N[1/(1-x), 600];
+  N[-(1/(288 sp^7)), 600]
 ];
 
 Jmax = 60;
@@ -147,35 +186,65 @@ M1[x_?NumericQ,J_?IntegerQ] := {
 	{0,0,0}
 };
 
-M2[x_?NumericQ, J_?IntegerQ] :={
+M41[x_?NumericQ, J_?IntegerQ] :={
 	{n4AAAA[x,J],0,0},
 	{0,0,0},
 	{0,0,0}
 };
 
-M3[x_?NumericQ, J_?IntegerQ] :={
-	{X52AAAA[x,J],0,0},
-	{0,0,0},
-	{0,0,0}
-};
-
-M4[x_?NumericQ] :={
-	{LargeJAAAA[x],0,0},
-	{0,0,0},
-	{0,0,0}
-}
-
-M5[x_?NumericQ, J_?IntegerQ] :={
+M42[x_?NumericQ, J_?IntegerQ] :={
 	{0,0,0},
 	{0,n4BBBB[x,J],0},
 	{0,0,0}
 };
 
-M6[x_?NumericQ, J_?IntegerQ] :={
+M43[x_?NumericQ, J_?IntegerQ] :={
   {0,1/2*n4BBAA[x,J],0},
   {1/2*n4BBAA[x,J],0,0},
   {0,0,n4ABAB[x,J]+n4ABBA[x,J]}
 };
+
+M51[x_?NumericQ, J_?IntegerQ] :={
+	{X52AAAA[x,J],0,0},
+	{0,0,0},
+	{0,0,0}
+};
+
+M52[x_?NumericQ, J_?IntegerQ] :={
+  {0,0,0},
+	{0,X52BBBB[x,J],0},
+	{0,0,0}
+};
+
+M53[x_?NumericQ, J_?IntegerQ] :={
+  {0,1/2*X52BBAA[x,J],0},
+  {1/2*X52BBAA[x,J],0,0},
+  {0,0,X52ABAB[x,J]+X52ABBA[x,J]}
+};
+
+M61[x_?NumericQ, J_?IntegerQ] :={
+	{X62AAAA[x,J],0,0},
+	{0,0,0},
+	{0,0,0}
+};
+
+M62[x_?NumericQ, J_?IntegerQ] :={
+  {0,0,0},
+	{0,X62BBBB[x,J],0},
+	{0,0,0}
+};
+
+M6j1[x_?NumericQ] :={
+	{LargeJAAAA[x],0,0},
+	{0,0,0},
+	{0,0,0}
+}
+
+M6j2[x_?NumericQ] :={
+	{0,0,0},
+	{0,LargeJBBBB[x],0},
+	{0,0,0}
+}
 
 (* null *)
 N0[x_?NumericQ] :={
@@ -187,37 +256,53 @@ N0[x_?NumericQ] :={
 f11List ={
 	Function[{x,J}, M0[x,J][[1,1]]],
 	Function[{x,J}, M1[x,J][[1,1]]],
-	Function[{x,J}, M2[x,J][[1,1]]],
-  Function[{x,J}, M5[x,J][[1,1]]],
-  Function[{x,J}, M6[x,J][[1,1]]],
-	Function[{x,J}, M3[x,J][[1,1]]]
+	Function[{x,J}, M41[x,J][[1,1]]],
+  Function[{x,J}, M42[x,J][[1,1]]],
+  Function[{x,J}, M43[x,J][[1,1]]],
+	Function[{x,J}, M51[x,J][[1,1]]],
+  Function[{x,J}, M52[x,J][[1,1]]],
+  Function[{x,J}, M53[x,J][[1,1]]],
+  Function[{x,J}, M61[x,J][[1,1]]],
+  Function[{x,J}, M62[x,J][[1,1]]]
 };
 
 f22List ={
 	Function[{x,J}, M0[x,J][[2,2]]],
 	Function[{x,J}, M1[x,J][[2,2]]],
-	Function[{x,J}, M2[x,J][[2,2]]],
-  Function[{x,J}, M5[x,J][[2,2]]],
-  Function[{x,J}, M6[x,J][[2,2]]],
-	Function[{x,J}, M3[x,J][[2,2]]]
+	Function[{x,J}, M41[x,J][[2,2]]],
+  Function[{x,J}, M42[x,J][[2,2]]],
+  Function[{x,J}, M43[x,J][[2,2]]],
+	Function[{x,J}, M51[x,J][[2,2]]],
+  Function[{x,J}, M52[x,J][[2,2]]],
+  Function[{x,J}, M53[x,J][[2,2]]],
+  Function[{x,J}, M61[x,J][[2,2]]],
+  Function[{x,J}, M62[x,J][[2,2]]]
 };
 
 f33List = {
 	Function[{x,J}, M0[x,J][[3,3]]],
 	Function[{x,J}, M1[x,J][[3,3]]],
-	Function[{x,J}, M2[x,J][[3,3]]],
-  Function[{x,J}, M5[x,J][[3,3]]],
-  Function[{x,J}, M6[x,J][[3,3]]],
-	Function[{x,J}, M3[x,J][[3,3]]]
+	Function[{x,J}, M41[x,J][[3,3]]],
+  Function[{x,J}, M42[x,J][[3,3]]],
+  Function[{x,J}, M43[x,J][[3,3]]],
+	Function[{x,J}, M51[x,J][[3,3]]],
+  Function[{x,J}, M52[x,J][[3,3]]],
+  Function[{x,J}, M53[x,J][[3,3]]],
+  Function[{x,J}, M61[x,J][[3,3]]],
+  Function[{x,J}, M62[x,J][[3,3]]]
 };
 
 f12List ={
 	Function[{x,J}, M0[x,J][[1,2]]],
 	Function[{x,J}, M1[x,J][[1,2]]],
-	Function[{x,J}, M2[x,J][[1,2]]],
-  Function[{x,J}, M5[x,J][[1,2]]],
-  Function[{x,J}, M6[x,J][[1,2]]],
-	Function[{x,J}, M3[x,J][[1,2]]]
+	Function[{x,J}, M41[x,J][[1,2]]],
+  Function[{x,J}, M42[x,J][[1,2]]],
+  Function[{x,J}, M43[x,J][[1,2]]],
+	Function[{x,J}, M51[x,J][[1,2]]],
+  Function[{x,J}, M52[x,J][[1,2]]],
+  Function[{x,J}, M53[x,J][[1,2]]],
+  Function[{x,J}, M61[x,J][[1,2]]],
+  Function[{x,J}, M62[x,J][[1,2]]]
 };
 
 f21List = f12List;
@@ -225,20 +310,28 @@ f21List = f12List;
 f13List = {
 	Function[{x,J}, M0[x,J][[1,3]]],
 	Function[{x,J}, M1[x,J][[1,3]]],
-	Function[{x,J}, M2[x,J][[1,3]]],
-  Function[{x,J}, M5[x,J][[1,3]]],
-  Function[{x,J}, M6[x,J][[1,3]]],
-	Function[{x,J}, M3[x,J][[1,3]]]
+	Function[{x,J}, M41[x,J][[1,3]]],
+  Function[{x,J}, M42[x,J][[1,3]]],
+  Function[{x,J}, M43[x,J][[1,3]]],
+	Function[{x,J}, M51[x,J][[1,3]]],
+  Function[{x,J}, M52[x,J][[1,3]]],
+  Function[{x,J}, M53[x,J][[1,3]]],
+  Function[{x,J}, M61[x,J][[1,3]]],
+  Function[{x,J}, M62[x,J][[1,3]]]
 };
 f31List = f13List;
 
 f23List = {
 	Function[{x,J}, M0[x,J][[2,3]]],
 	Function[{x,J}, M1[x,J][[2,3]]],
-	Function[{x,J}, M2[x,J][[2,3]]],
-  Function[{x,J}, M5[x,J][[2,3]]],
-  Function[{x,J}, M6[x,J][[2,3]]],
-	Function[{x,J}, M3[x,J][[2,3]]]
+	Function[{x,J}, M41[x,J][[2,3]]],
+  Function[{x,J}, M42[x,J][[2,3]]],
+  Function[{x,J}, M43[x,J][[2,3]]],
+	Function[{x,J}, M51[x,J][[2,3]]],
+  Function[{x,J}, M52[x,J][[2,3]]],
+  Function[{x,J}, M53[x,J][[2,3]]],
+  Function[{x,J}, M61[x,J][[2,3]]],
+  Function[{x,J}, M62[x,J][[2,3]]]
 };
 f32List = f23List;
 
@@ -248,7 +341,11 @@ j11List = {
 	Function[{x}, N0[x][[1,1]]],
   Function[{x}, N0[x][[1,1]]],
   Function[{x}, N0[x][[1,1]]],
-	Function[{x}, M4[x][[1,1]]]
+  Function[{x}, N0[x][[1,1]]],
+  Function[{x}, N0[x][[1,1]]],
+  Function[{x}, N0[x][[1,1]]],
+  Function[{x}, M61j[x][[1,1]]],
+	Function[{x}, M62j[x][[1,1]]]
 };
 j22List = {
 	Function[{x}, N0[x][[2,2]]],
@@ -256,7 +353,11 @@ j22List = {
 	Function[{x}, N0[x][[2,2]]],
   Function[{x}, N0[x][[2,2]]],
   Function[{x}, N0[x][[2,2]]],
-	Function[{x}, M4[x][[2,2]]]
+  Function[{x}, N0[x][[2,2]]],
+  Function[{x}, N0[x][[2,2]]],
+  Function[{x}, N0[x][[2,2]]],
+  Function[{x}, M61j[x][[2,2]]],
+	Function[{x}, M62j[x][[2,2]]]
 };
 j33List = {
   Function[{x}, N0[x][[3,3]]],
@@ -264,7 +365,11 @@ j33List = {
 	Function[{x}, N0[x][[3,3]]],
 	Function[{x}, N0[x][[3,3]]],
   Function[{x}, N0[x][[3,3]]],
-	Function[{x}, M4[x][[3,3]]]
+  Function[{x}, N0[x][[3,3]]],
+  Function[{x}, N0[x][[3,3]]],
+  Function[{x}, N0[x][[3,3]]],
+  Function[{x}, M61j[x][[3,3]]],
+	Function[{x}, M62j[x][[3,3]]]
 };
 
 j12List = {
@@ -273,7 +378,11 @@ j12List = {
 	Function[{x}, N0[x][[1,2]]],
 	Function[{x}, N0[x][[1,2]]],
   Function[{x}, N0[x][[1,2]]],
-	Function[{x}, M4[x][[1,2]]]
+  Function[{x}, N0[x][[1,2]]],
+  Function[{x}, N0[x][[1,2]]],
+  Function[{x}, N0[x][[1,2]]],
+  Function[{x}, M61j[x][[1,2]]],
+	Function[{x}, M62j[x][[1,2]]]
 };
 j21List = j12List;
 
@@ -283,7 +392,11 @@ j13List = {
 	Function[{x}, N0[x][[1,3]]],
 	Function[{x}, N0[x][[1,3]]],
   Function[{x}, N0[x][[1,3]]],
-	Function[{x}, M4[x][[1,3]]]
+  Function[{x}, N0[x][[1,3]]],
+  Function[{x}, N0[x][[1,3]]],
+  Function[{x}, N0[x][[1,3]]],
+  Function[{x}, M61j[x][[1,3]]],
+	Function[{x}, M62j[x][[1,3]]]
 };
 j31List = j13List;
 
@@ -293,7 +406,11 @@ j23List = {
 	Function[{x}, N0[x][[2,3]]],
 	Function[{x}, N0[x][[2,3]]],
   Function[{x}, N0[x][[2,3]]],
-	Function[{x}, M4[x][[2,3]]]
+  Function[{x}, N0[x][[2,3]]],
+  Function[{x}, N0[x][[2,3]]],
+  Function[{x}, N0[x][[2,3]]],
+  Function[{x}, M61j[x][[2,3]]],
+	Function[{x}, M62j[x][[2,3]]]
 };
 j32List = j23List;
 
@@ -302,8 +419,8 @@ j32List = j23List;
 (* extraTriplet = {0&, 0&, 0&, LargeJ}; *)
 
 (* optimal upper bound *)
-norm = {0, -1, 0, 0, 0, 0};
-obj  = {-1, 0, 0, 0, 0, 0};
+norm = {0, -1, 0, 0, 0, 0, 0, 0, 0, 0};
+obj  = {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 
 testNumericalSDP[spFile_String, jsonFile_String, prec_:600] := Module[
