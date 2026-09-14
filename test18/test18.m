5e821b4d295e3a41ac85a3e4c85c7b062b7cd4b3
tingkaihsu <tingkaihsu0924@gmail.com>
2026-09-14 15:37:04 +0800

    fix minus sign

--- test18/test18.m
+++ test18/test18.m
@@ -302,8 +302,8 @@ PMP2SDP[datfile_, prec_: 600] := Module[
 
   pols = Join[specialBlocks, continuumBlocks, largeJBlocks];
 
-  norm = -N[Flatten[{{0, 1}, list0}], prec];
-  obj = -N[Flatten[{{1, 0}, list0}], prec];
+  norm = N[Flatten[{{0, 1}, list0}], prec];
+  obj = N[Flatten[{{1, 0}, list0}], prec];
 
   If[Length[norm] =!= functionalCount || Length[obj] =!= functionalCount,
     Print["Objective or normalization dimension mismatch."];
